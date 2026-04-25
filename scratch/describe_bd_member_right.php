039e4afb89a5e83a90fb233d532b4a35b0bea80b
Your Name <wlstjrl3@naver.com>
2026-04-25 09:16:26 +0900

    Ignore and untrack scratch directory

--- scratch/describe_bd_member_right.php
+++
@@ -1,4 +0,0 @@
-<?php
-$pdo = new PDO('mysql:host=localhost;dbname=CTMS', 'root', '');
-$stmt = $pdo->query('DESCRIBE bd_member_right');
-while($row = $stmt->fetch()) { echo $row[0] . ' (' . $row[1] . ')' . PHP_EOL; }
